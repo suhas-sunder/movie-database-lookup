ead48a0ed47a2bb297f1a2a2ed7cb4cc63ce464e
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-08-28 22:56:06 -0400

    fix: re-installed packages & re-configured init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Components
+import Home from "./routes/Home";
+import UpdatePage from "./routes/UpdatePage";
+import InfoPage from "./routes/InfoPage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/info" element={<InfoPage />} />
+      <Route path="/update" element={<UpdatePage />} />
+      <Route path="*" element={<h2>404 Not Found!</h2>} />
+    </Routes>
   );
 }
 
---
+++ client/src/routes/InfoPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function InfoPage() {
+  return <div>InfoPage</div>;
+}
+
+export default InfoPage;
